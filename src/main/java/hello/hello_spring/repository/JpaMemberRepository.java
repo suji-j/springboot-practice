9b91ee4cf01b0bcf57e19664454e5ffbb27f8063
SUJI-J <suzy852@naver.com>
2025-02-04 17:48:18 +0900

    fix: 메서드명 오타 수정

--- src/main/java/hello/hello_spring/repository/JpaMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/JpaMemberRepository.java
@@ -20,7 +20,7 @@ public class JpaMemberRepository implements MemberRepository {
     }
 
     @Override
-    public Optional<Member> fineById(Long id) {
+    public Optional<Member> findById(Long id) {
         Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
     }
