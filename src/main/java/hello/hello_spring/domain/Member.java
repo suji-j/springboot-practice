3fb5f1f0337cbee3c8bfe0523015efa3d9d8d7a7
SUJI-J <suzy852@naver.com>
2025-02-04 16:23:13 +0900

    feat: Member Entity에 ID 자동 생성 전략 추가

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -1,7 +1,12 @@
 package hello.hello_spring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     public void setId(Long id) {
         this.id = id;
